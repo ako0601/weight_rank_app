3b5191dd4f0811eb25e2cd42ff409faa0c428869
Kwangok Ahn <dksrhkddhr06@gmail.com>
2021-05-11 20:23:39 +0900

    홈페이지 구성

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -31,6 +31,9 @@ class _HomeScreenState extends State<HomeScreen> {
     return ListView(
       children: <Widget>[
         Logo(),
+        Post(),
+        Post(),
+        Post(),
       ],
     );
   }
@@ -75,6 +78,145 @@ class Logo extends StatelessWidget {
                 IconButton(
                   icon: Icon(Icons.add),
                   onPressed: null,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Post extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 20,
+                        backgroundImage:
+                            AssetImage('./images/profile_dummy.jpg'),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(left: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('12',
+                                style: TextStyle(
+                                    fontSize: 11, color: Colors.black)),
+                            Text('Michel Cooper',
+                                style: TextStyle(
+                                    fontSize: 18, color: Colors.black)),
+                            Text('MVP Gym',
+                                style: TextStyle(
+                                    fontSize: 11, color: Colors.black)),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Icon(
+                  Icons.my_library_add_outlined,
+                  color: Colors.black,
+                ),
+              ],
+            ),
+          ),
+          Row(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(left: 10, right: 10),
+                    child: Text('Chest',
+                        style: TextStyle(fontSize: 18, color: Colors.black)),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(left: 20, right: 20),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Bench Press (10reps / 4sets)',
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 15)),
+                        Text('Incline Bench Press (8reps / 4sets)',
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 15)),
+                        Text('Decline Bench Press (8reps / 4sets)',
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 15)),
+                        Text('Flat Dumbell Bench Press (8reps / 4sets)',
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 15)),
+                        Text('Incline Dumbbell Bench Press (8reps / 4sets)',
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 15)),
+                        Text('Decline Dumbbell Bench Press (8reps / 3sets)',
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 15)),
+                        Text('Push Ups (12reps / 5sets)',
+                            style: TextStyle(color: Colors.black, fontSize: 15))
+                      ],
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 20, bottom: 5),
+            child: Row(
+              children: [
+                Image.asset(
+                  'images/rope.jpg',
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.width,
+                  fit: BoxFit.cover,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 10, right: 10),
+            margin: EdgeInsets.only(bottom: 5),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Icon(
+                      Icons.favorite_outline_rounded,
+                      color: Colors.black,
+                    ),
+                    Icon(
+                      Icons.mode_comment_outlined,
+                      color: Colors.black,
+                    ),
+                    Icon(
+                      Icons.ios_share,
+                      color: Colors.black,
+                    ),
+                  ],
+                ),
+                Icon(
+                  Icons.bookmark_border_rounded,
+                  color: Colors.black,
                 ),
               ],
             ),
